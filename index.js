81d006c08836892a84bba9ce72ec63e0dd9aaf10
matteo78 <quelquonque@hotmail.fr>
2014-12-30 15:53:32 +0100

    :on: Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*jshint node:true, browser:true */
+
+'use strict';
+
+exports.connect = require('./connect');
+exports.File = require('./file');
+exports.programs = require('./programs');
+
+exports.keyOffset = 21;

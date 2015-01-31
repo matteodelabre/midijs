1f7e82a5460d54a3d567d91b122552bcc242ddf3
MattouFP <quelquonque@hotmail.fr>
2015-01-31 11:49:50 +0100

    :bug: Fix path, remove undocumented value

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 'use strict';
 
 exports.Reader = require('./lib/parser/reader').Reader;
-exports.Writer = require('./lib/parser/writer').Writer;
+exports.Writer = require('./lib/encoder/writer').Writer;
 
 exports.connect = require('./lib/driver/connect');
 exports.programs = require('./lib/programs');
-
-exports.keyOffset = 21;

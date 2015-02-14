3512236ff4102bd8338b561f3320364abc507610
MattouFP <matteo.delabre@outlook.fr>
2015-02-14 12:01:01 +0100

    :bug: Fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-exports.Reader = require('./lib/parser/reader').Reader;
-exports.Writer = require('./lib/encoder/writer').Writer;
-
-exports.connect = require('./lib/driver/connect');
-exports.programs = require('./lib/programs');
+exports.File = require('./lib/file').File;
+exports.connect = require('./lib/connect').connect;
+exports.programs = require('./lib/programs').programs;

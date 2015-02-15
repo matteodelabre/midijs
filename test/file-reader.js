914b6eb3a58485c6b64790d04300533d25928856
MattouFP <matteo.delabre@outlook.fr>
2015-02-16 00:23:25 +0100

    :bulb: Change fixture file

--- test/file-reader.js
+++ test/file-reader.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert');
 var path = require('path');
+var buffer = require('buffer');
 var fs = require('fs');
 
 var MIDI = require('../index');
@@ -64,104 +65,139 @@ describe('File as a reader', function () {
         });
         
         it('should parse header with correct data', function () {
-            var header = new Header(1, 2, 480);
+            var header = new Header(1, 2, 120);
             assert.deepEqual(file.header, header);
         });
         
         it('should parse tracks with correct data', function () {
             var tracks = [
-                new Track(
-                    new MetaEvent(MetaEvent.TYPE.INSTRUMENT_NAME, {
-                        text: ''
+                new Track([
+                    new MetaEvent(MetaEvent.TYPE.SEQUENCE_NUMBER, {
+                        number: 0
+                    }),
+                    new MetaEvent(MetaEvent.TYPE.SEQUENCE_NAME, {
+                        text: 'Meta track'
+                    }),
+                    new MetaEvent(MetaEvent.TYPE.COPYRIGHT_NOTICE, {
+                        text: '© 2015, Mattéo DELABRE'
+                    }),
+                    new MetaEvent(MetaEvent.TYPE.TEXT, {
+                        text: 'A fixture for testing all types ' +
+                              'of MIDI events in one file'
+                    }),
+                    new MetaEvent(MetaEvent.TYPE.TIME_SIGNATURE, {
+                        numerator: 2,
+                        denominator: 4,
+                        metronome: 24,
+                        clockSignalsPerBeat: 8
+                    }),
+                    new MetaEvent(MetaEvent.TYPE.KEY_SIGNATURE, {
+                        major: false,
+                        note: 3
+                    }),
+                    new MetaEvent(MetaEvent.TYPE.SEQUENCER_SPECIFIC, {
+                        data: new buffer.Buffer('just testing out')
+                    }),
+
+                    new MetaEvent(MetaEvent.TYPE.SET_TEMPO, {
+                        tempo: 240
                     }),
                     new MetaEvent(MetaEvent.TYPE.SET_TEMPO, {
-                        tempo: 120
+                        tempo: 480
+                    }, 600),
+
+                    new MetaEvent(MetaEvent.TYPE.END_OF_TRACK)
+                ]),
+                new Track([
+                    new MetaEvent(MetaEvent.TYPE.SEQUENCE_NUMBER, {
+                        number: 1
                     }),
                     new MetaEvent(MetaEvent.TYPE.SEQUENCE_NAME, {
-                        text: 'Sequence Name'
+                        text: 'Test song'
                     }),
-                    new MetaEvent(MetaEvent.TYPE.END_OF_TRACK)
-                ),
-                new Track(
+
+                    new MetaEvent(MetaEvent.TYPE.CHANNEL_PREFIX, {
+                        channel: 0
+                    }),
                     new MetaEvent(MetaEvent.TYPE.INSTRUMENT_NAME, {
-                        text: 'Acoustic Grand Piano'
+                        text: 'Church organ'
                     }),
-                    new MetaEvent(MetaEvent.TYPE.SEQUENCE_NAME, {
-                        text: 'My New Track'
-                    }),
-                    new ChannelEvent(ChannelEvent.TYPE.CONTROLLER, {
-                        controller: 7,
-                        value: 127
-                    }, 0),
                     new ChannelEvent(ChannelEvent.TYPE.PROGRAM_CHANGE, {
-                        program: 1
+                        program: MIDI.programs.indexOf('organ_church')
                     }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
-                        note: 64,
-                        velocity: 127
-                    }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
-                        note: 64,
-                        velocity: 127
-                    }, 0, 480),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
-                        note: 66,
-                        velocity: 127
-                    }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
-                        note: 66,
-                        velocity: 127
-                    }, 0, 480),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
-                        note: 68,
-                        velocity: 127
-                    }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
-                        note: 68,
-                        velocity: 127
-                    }, 0, 480),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
-                        note: 69,
-                        velocity: 127
-                    }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
-                        note: 69,
-                        velocity: 127
-                    }, 0, 480),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
-                        note: 71,
-                        velocity: 127
-                    }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
-                        note: 71,
-                        velocity: 127
-                    }, 0, 480),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
-                        note: 73,
-                        velocity: 127
-                    }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
-                        note: 73,
-                        velocity: 127
-                    }, 0, 480),
+
+                    new MetaEvent(MetaEvent.TYPE.SMPTE_OFFSET, {
+                        rate: 25,
+                        hours: 15,
+                        minutes: 53,
+                        seconds: 10,
+                        frames: 20,
+                        subframes: 50
+                    }),
+
                     new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
                         note: 75,
                         velocity: 127
                     }, 0),
+                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
+                        note: 60,
+                        velocity: 127
+                    }, 0, 120),
+                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
+                        note: 60,
+                        velocity: 127
+                    }, 0),
+
+                    new MetaEvent(MetaEvent.TYPE.LYRICS, {
+                        text: 'test'
+                    }),
+                    new MetaEvent(MetaEvent.TYPE.SET_TEMPO, {
+                        tempo: 60
+                    }),
+
+                    new ChannelEvent(ChannelEvent.TYPE.NOTE_AFTERTOUCH, {
+                        note: 75,
+                        pressure: 50
+                    }, 0, 480),
+                    new ChannelEvent(ChannelEvent.TYPE.NOTE_AFTERTOUCH, {
+                        note: 60,
+                        pressure: 50
+                    }, 0),
+                    new ChannelEvent(ChannelEvent.TYPE.NOTE_AFTERTOUCH, {
+                        note: 60,
+                        pressure: 50
+                    }, 0),
+
+                    new ChannelEvent(ChannelEvent.TYPE.CHANNEL_AFTERTOUCH, {
+                        pressure: 127
+                    }, 0, 480),
+
+                    new MetaEvent(MetaEvent.TYPE.MARKER, {
+                        text: 'Pitch bend'
+                    }, 480),
+                    new ChannelEvent(ChannelEvent.TYPE.PITCH_BEND, {
+                        value: -6000
+                    }, 0),
+
                     new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
                         note: 75,
                         velocity: 127
                     }, 0, 480),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_ON, {
-                        note: 76,
-                        velocity: 127
-                    }, 0),
-                    new ChannelEvent(ChannelEvent.TYPE.NOTE_OFF, {
-                        note: 76,
-                        velocity: 127
+
+                    new ChannelEvent(ChannelEvent.TYPE.CONTROLLER, {
+                        controller: 123,
+                        value: 0
                     }, 0, 480),
+
+                    new MetaEvent(MetaEvent.TYPE.CUE_POINT, {
+                        text: 'All sounds are stopped'
+                    }, 480),
+                    new ChannelEvent(ChannelEvent.TYPE.PROGRAM_CHANGE, {
+                        program: 0
+                    }, 0),
+
                     new MetaEvent(MetaEvent.TYPE.END_OF_TRACK)
-                )
+                ])
             ];
             
             file.tracks.forEach(function (track, i) {

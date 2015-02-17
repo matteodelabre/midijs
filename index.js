d074d9dfe688f4f39b2439fdbb57e77865d7fa56
MattouFP <matteo.delabre@outlook.fr>
2015-02-17 13:11:44 +0100

    :book: Documentation tweaks

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+/**
+ * @module midijs
+ */
+
 'use strict';
 
+/**
+ * Parse and encode MIDI data<br />
+ * {@link module:midijs/lib/file}
+ */
 exports.File = require('./lib/file').File;
+
+/**
+ * Establish a connection to the MIDI driver<br />
+ * {@link module:midijs/lib/connect}
+ */
 exports.connect = require('./lib/connect').connect;
+
+/**
+ * List of channel programs defined by the General MIDI standard<br />
+ * {@link module:midijs/lib/programs}
+ */
 exports.programs = require('./lib/programs').programs;
+
+/**
+ * Constructors of the errors that can be thrown by this module<br />
+ * {@link module:midijs/lib/error}
+ */
+exports.error = require('./lib/error');

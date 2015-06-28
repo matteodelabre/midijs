86da580e8f03b02efbd62e1d4ce655f2f5193a7a
mattoufp <matteo.delabre@outlook.fr>
2015-06-28 12:36:24 +0200

    :book: Improve documentation

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 'use strict';
 
-/**
- * Parse and encode MIDI data<br />
- * {@link module:midijs/lib/file}
- */
 exports.File = require('./lib/file').File;
-
-/**
- * Establish a connection to the MIDI driver<br />
- * {@link module:midijs/lib/connect}
- */
 exports.connect = require('./lib/connect').connect;
-
-/**
- * List of programs and instruments defined by the General MIDI standard<br />
- * {@link module:midijs/lib/gm}
- */
 exports.gm = require('./lib/gm');
-
-/**
- * Constructors of the errors that can be thrown by this module<br />
- * {@link module:midijs/lib/error}
- */
 exports.error = require('./lib/error');

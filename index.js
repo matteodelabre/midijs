c41134a8790560505145f32e5598a37e74d68b7e
mattoufp <matteo.delabre@outlook.fr>
2015-08-01 20:06:29 +0200

    :bulb: Update entry point

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 exports.File = require('./lib/file/file');
+exports.Event = require('./lib/event/event');
+
 // exports.connect = require('./lib/connect').connect; TODO
 exports.gm = require('./lib/gm');
 exports.error = require('./lib/util/error');

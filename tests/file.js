d6f300a985e239da46357bed99f007fe4070f845
mattoufp <matteo.delabre@outlook.fr>
2015-08-17 00:37:42 +0200

    :bulb: Extend file test to include new shortcut

--- tests/file.js
+++ tests/file.js
@@ -98,15 +98,30 @@ test('File', function (sub) {
                     text: 'testing'
                 })
                 .sysex('type 1', new Buffer('void'))
-                .end(),
+                .note('E4+E5', 120, 1, 5, 103)
+            .end(),
         file, 'should allow chaining');
 
         assert.equal(file.tracks.length, 1, 'should create a track');
-        assert.equal(file.tracks[0].events.length, 4, 'should create events');
+        assert.equal(file.tracks[0].events.length, 8, 'should create events');
         assert.ok(file.tracks[0].events[0] instanceof events.ChannelEvent, 'should create channel events');
         assert.ok(file.tracks[0].events[1] instanceof events.MetaEvent, 'should create meta events');
         assert.ok(file.tracks[0].events[2] instanceof events.SysexEvent, 'should create sysex events');
-        assert.equal(file.tracks[0].events[3].type, events.MetaEvent.TYPE.END_OF_TRACK, 'should append end event');
+        assert.ok(file.tracks[0].events[3] instanceof events.ChannelEvent, 'should allow shortcut note notation');
+        assert.ok(file.tracks[0].events[4] instanceof events.ChannelEvent);
+        assert.ok(file.tracks[0].events[5] instanceof events.ChannelEvent);
+        assert.ok(file.tracks[0].events[6] instanceof events.ChannelEvent);
+        assert.equal(file.tracks[0].events[3].type, events.ChannelEvent.TYPE.NOTE_ON);
+        assert.equal(file.tracks[0].events[4].type, events.ChannelEvent.TYPE.NOTE_ON);
+        assert.equal(file.tracks[0].events[5].type, events.ChannelEvent.TYPE.NOTE_OFF);
+        assert.equal(file.tracks[0].events[6].type, events.ChannelEvent.TYPE.NOTE_OFF);
+        assert.equal(file.tracks[0].events[3].delay, 5);
+        assert.equal(file.tracks[0].events[5].delay, 120);
+        assert.equal(file.tracks[0].events[3].channel, 1);
+        assert.equal(file.tracks[0].events[4].channel, 1);
+        assert.equal(file.tracks[0].events[3].data.velocity, 103);
+        assert.equal(file.tracks[0].events[4].data.velocity, 103);
+        assert.equal(file.tracks[0].events[7].type, events.MetaEvent.TYPE.END_OF_TRACK, 'should append end event');
 
         assert.end();
     });

c608150b09ed517082652ee55f6c94a459bc0231
mattoufp <contact@matteodelabre.me>
2015-08-31 19:20:29 +0200

    :bulb: Improve buffer-equal assertion

--- tests/util/buffer-equal.js
+++ tests/util/buffer-equal.js
@@ -1,28 +1,57 @@
 'use strict';
 
-var MIDIBuffer = require('../../lib/util/buffer');
+/**
+ * Format a byte to hexadecimal
+ *
+ * @param {number} byte Value of the byte
+ * @return {string} Hex value of that byte
+ */
+function formatByte(byte) {
+    return '0x' + byte.toString(16);
+}
 
 /**
- * Check whether two buffers contain the same data or not
+ * Assert that two buffer equal each other
  *
- * @param {MIDIBuffer|Buffer|Array|*} ina First buffer
- * @param {MIDIBuffer|Buffer|Array|*} inb Second buffer
- * @return {bool} Whether the two buffers equal or not
+ * @param {Object} test Tape testing object
+ * @param {Object|Buffer|Array} input1 First buffer
+ * @param {Object|Buffer|Array} input2 Second buffer
+ * @param {string} msg Description for this assertion
+ * @param {Object} extra Extra data
  */
-module.exports = function bufferEqual(ina, inb) {
-    var a = new MIDIBuffer(ina).toBuffer(),
-        b = new MIDIBuffer(inb).toBuffer(),
-        length = a.length, i;
+module.exports = function bufferEqual(test, input1, input2, msg, extra) {
+    var buffer1, buffer2, equals = true,
+        length, i, actual, expected;
 
-    if (length !== b.length) {
-        return false;
-    }
+    // convert inputs to buffers
+    buffer1 = (input1 instanceof Buffer) ? input1 : new Buffer(input1);
+    buffer2 = (input2 instanceof Buffer) ? input2 : new Buffer(input2);
+
+    length = Math.max(buffer1.length, buffer2.length);
 
     for (i = 0; i < length; i += 1) {
-        if (a[i] !== b[i]) {
-            return false;
+        if (buffer1[i] !== buffer2[i]) {
+            equals = false;
         }
     }
 
-    return true;
+    // build diffing strings if the buffers differ
+    if (!equals) {
+        buffer1 = [].slice.call(buffer1);
+        buffer2 = [].slice.call(buffer2);
+
+        actual = '<Buffer (' + buffer1.length + ') ' +
+            buffer1.map(formatByte).join(' ') + '>';
+
+        expected = '<Buffer (' + buffer2.length + ') ' +
+            buffer2.map(formatByte).join(' ') + '>';
+    }
+
+    test._assert(equals, {
+        message: (msg === undefined) ? 'should be equal' : msg,
+        operator: 'equal',
+        actual: actual,
+        expected: expected,
+        extra: extra
+    });
 };
